07ea0ddaad3e2a07cf072429a4ca812582f9eedf
Alin Babu <alin@qburst.com>
2020-11-02 09:54:12 +0530

    Adding the title fragment

---
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -0,0 +1,25 @@
+package com.example.android.navigation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.android.navigation.databinding.FragmentTitleBinding
+
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class TitleFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        val binding = DataBindingUtil.inflate<FragmentTitleBinding>(inflater,
+                R.layout.fragment_title,container,false)
+        return binding.root
+    }
+
+}
